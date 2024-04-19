b9b3ca46570bcf611e1545d787dde28754a77dcd
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2024-04-19 12:00:52 +0530

    Create Max.java

---
+++ Java/day3/Max.java
@@ -0,0 +1,12 @@
+public class Max {
+    public static void main(String[] args) {
+        int n1=100, n2=20;
+
+        if(n1>n2){
+            System.out.println("n1 is max " + n1);
+        }
+        else{
+            System.out.println("n2 is max " + n2);
+        }
+    }
+}
