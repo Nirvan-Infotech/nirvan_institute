49a566c34a76a51974d989fe0f5fee4c850138a5
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2024-04-19 11:59:59 +0530

    Create Relational.java, day2.txt, IncDec.java

---
+++ Java/day2/IncDec.java
@@ -0,0 +1,29 @@
+public class IncDec {
+    public static void main(String args[]){
+        // post increment
+        // int a = 10;
+        // a++; // 11
+        // System.out.println(a++); // 12
+        // System.out.println(a++); // 13
+        // System.out.println(a); // 13
+        // // post decrement
+        // int b = 10;
+        // b--; // 9
+        // System.out.println(b--); // 9
+        // System.out.println(b--); // 8
+        // System.out.println(b); // 7
+
+        // post increment
+        int a = 10;
+        ++a; // 11
+        System.out.println(++a); // 12
+        System.out.println(++a); // 13
+        System.out.println(a); // 13
+        // post decrement
+        int b = 10;
+       --b; // 9
+        System.out.println(--b); // 8
+        System.out.println(--b); // 7
+        System.out.println(b); // 7
+    }
+}
---
+++ Java/day2/Relational.java
@@ -0,0 +1,35 @@
+public class Relational {
+    public static void main(String[] args) {
+        /*
+         * == -> is equal to
+         * != -> not equal to
+         * > -> greater than 
+         * >= -> greater than or equal to
+         * < -> less than
+         * <= -> less than or equal to
+         */
+
+        //  conditional statements
+        /*
+         *  1. IF
+         *      -> if (1 condition)
+         *      -> if...else (2 conditions)
+         *      -> else if...else (>2 conditions)
+         *      -> if...else ladder (condition inside condition)
+         *  2. Switch...Case
+         */
+
+         int age = 15;
+
+         if(age >= 18){
+            System.out.println("You can vote");
+         }
+         else{
+            System.out.println("You can't vote.");
+         }
+
+        //  price = 85.25
+        // rupees = 85
+        // paise = 25
+    }
+}
