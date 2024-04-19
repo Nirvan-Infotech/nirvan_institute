8fc019ddcdf325f13f1938e063001ea64ad27822
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2024-04-19 11:58:19 +0530

    added Variable & Operators file

---
+++ Java/day1/Operators.java
@@ -0,0 +1,27 @@
+public class Operators {
+    public static void main(String[] args) {
+        /*
+        * Arithmatic Operators
+        * Increment / Decrement
+        * Assignment
+        * Relational
+        * Logical
+        * Bitwise
+         */
+
+        //  Arithmatic Operators (+ - * / % ^)
+        int a = 10, b = 20;
+        /*
+         * () {} []
+         * ^
+         * * /
+         * + -
+         */
+        System.out.println("A+B = " + (a+b));
+        System.out.println("A-B = " + (a-b));
+        System.out.println("A*B = " + (a*b));
+        System.out.println("A/B = " + (a/b)); // quotient -> double
+        System.out.println("A%B = " + (a%b)); // reminder -> int
+        System.out.println("A^B " + (a^b));
+    }
+}
---
+++ Java/day1/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String[] args) {
+        // data_type var_name;
+        // var_name = value;
+
+        int age;
+        age = 10;
+
+        // data_type var_name = value;
+
+        final double pi = 3.15;
+
+        System.out.println("Age: " + age);
+        System.out.println("PI: " + pi);
+    }
+}
