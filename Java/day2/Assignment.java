6d4d983f1b591e0aadb27c7bb80b6c1bf6cccd18
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2024-04-19 11:59:18 +0530

    Create Assignment.java

---
+++ Java/day2/Assignment.java
@@ -0,0 +1,25 @@
+public class Assignment {
+    /*
+     * Assignment Operators 
+     * a = a + b -> a+=b
+     * a + b -> 1
+     * a = a + b
+     * 
+     * a+=b
+     * a-=b
+     * 
+     * a>>=b
+     * a<<=b
+     * a^=b
+     * a|=b
+     * a&=b
+     */
+
+     public static void main(String[] args) {
+        int marks = 60;
+        int inc = 5;
+
+        marks += inc;
+        System.out.println(marks);
+     }
+}
